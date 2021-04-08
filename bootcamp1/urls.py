dacce7560f575271db2209086e996987c557d7d0
joao-dias12 <jpgd94@poli.ufrj.br>
2021-04-08 13:24:18 -0300

    Aula 2 sem modicações da url e da view

--- bootcamp1/urls.py
+++ bootcamp1/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from products import views
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('search/', views.home_view),
+    path('products/1/', views.product_detail_view),
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from products.models import Product
 
-# Create your views here.
+def home_view(request, *args, **kwargs):
+
+    return HttpResponse("<h1> Hello world</h1>")
+def product_detail_view(request, *args, **kwargs):
+    obj = Product.objects.get(id=1)
+    return HttpResponse(f"Product id {obj.id}")
